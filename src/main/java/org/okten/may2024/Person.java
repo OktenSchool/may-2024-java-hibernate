310e6c8df3f017163ae30a51428521dd3150a7ed
EOkten <eduardokten@gmail.com>
2024-12-18 21:42:45 +0200

    lesson 7 (Maven, JDBC)

---
+++ src/main/java/org/okten/may2024/Person.java
@@ -0,0 +1,13 @@
+package org.okten.may2024;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+
+    private Integer id;
+
+    private String name;
+}
